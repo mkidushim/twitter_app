a083e1ebf30669aa4fe978ed8510336207a20245
mkidushim <mkidushim@gmail.com>
2015-07-15 08:34:34 -0700

    first commit

---
+++ twitter.js
@@ -0,0 +1,21 @@
+function twitter_click (){
+  $.ajax({
+            url: 'twitter.php',
+            dataType: 'json',
+            cache: false,
+            crossDomain: true,
+            success: function(response) {
+            for(var i= 0; i< response.statuses.length; i++){
+                $('#container').append("<p>"+response.statuses[i].user.screen_name+"</p><br><p>"+response.statuses[i].text+"</p><br>");
+                console.log(response.statuses[i].text);
+            console.log(response.statuses[i].user.screen_name);
+            }
+            console.log(response.statuses[0].text);
+            console.log(response.statuses[0].user.screen_name);
+            
+            }
+        });  
+}
+$(document).ready(function(){
+
+})
---
+++ twitter.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once("twitteroauth.php"); //Path to twitteroauth library you downloaded in step 3
+
+$twitteruser = "sendgrid"; //user name you want to reference
+$notweets = 10; //how many tweets you want to retrieve
+$consumerkey = "pPuY4dUH1G5ESzN2VT5RgZSJX"; //Noted keys from step 2
+$consumersecret = "zVypjbhxqR1NEX4h15w662gbIP5rLhuFaoE3dy5PJN2Grm10y1"; //Noted keys from step 2
+$accesstoken = "395706513-uC7GAKDVpnr1cdI4IBUQqLZuSeQXvWciQiGLaM8i"; //Noted keys from step 2
+$accesstokensecret = "t1pyAPrnsvIo87m18eRdlVc4rBb6Oa2Lyq3NCxByJZ8h9"; //Noted keys from step 2
+
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+
+$tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=".$twitteruser."&count=".$notweets);
+
+echo json_encode($tweets);
+echo $tweets; //testing remove for production   
+?>
